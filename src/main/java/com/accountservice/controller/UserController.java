86609f2f03aa90bf0d2fcc05077c1e34225023da
Joel <acostajoel0001@gmail.com>
2024-04-11 21:51:56 -0500

    feat: add endpoints list and getUser

--- src/main/java/com/accountservice/controller/UserController.java
+++ src/main/java/com/accountservice/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.accountservice.controller;
 
+import com.accountservice.domain.user.DataResponseUser;
 import com.accountservice.domain.user.DataSaveUser;
 import com.accountservice.domain.user.UserEntity;
 import com.accountservice.domain.user.UserRepository;
 import com.accountservice.infra.error.DataResponseSuccessfully;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -26,5 +30,17 @@ public class UserController {
         URI uri = uriComponentsBuilder.path("/api/v1/users/{id}").buildAndExpand(userEntity.getUser_id()).toUri();
 
         return ResponseEntity.created(uri).body(new DataResponseSuccessfully(HttpStatus.CREATED, "User " + userEntity.getUsername() + "has been created successfully"));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DataResponseUser>> listUsers(Pageable pageable) {
+        return ResponseEntity.ok(userRepository.findAll(pageable).map(DataResponseUser::new));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataResponseUser> getUserData(@PathVariable UUID id) {
+        UserEntity userEntity = userRepository.findById(id).get();
+
+        return ResponseEntity.ok(new DataResponseUser(userEntity));
     }
 }
